acb113ddc6e03eebff4978b721755bb7bc824782
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-03-05 14:11:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+const submitBtn=document.getElementById('submit-btn');
+const url=document.getElementById('url');
+const name=document.getElementById('name');
+const year=document.getElementById('year');
+
+submitBtn.addEventListener('click',(e)=>{
+	e.preventDefault();
+	if(name.value!==''){
+		url.textContent+=`?name=${name.value}`
+		if(year.value!==''){
+			url.textContent+=`&year=${year.value}`
+		}
+	}else if(year.value!==''){
+		url.textContent+=`?year=${year.value}`
+	}
+})
