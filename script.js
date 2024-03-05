ec80ab1433d459437d33cf494fbf5a44bcbd5167
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-03-05 14:54:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // your code here
 const urlDefaultText='https://localhost:8080/';
-const form=document.getElementById('form');
+// const form=document.getElementById('form');
+const formBtn=document.getElementById('button');
 const url=document.getElementById('url');
-// const name=document.getElementById('name');
-// const year=document.getElementById('year');
+const name=document.getElementById('name');
+const year=document.getElementById('year');
 
-form.addEventListener('submit',(e)=>{
+formBtn.addEventListener('click',(e)=>{
 	e.preventDefault();
-	const {name,year}=e.target
+	// const {name,year}=e.target
 	url.textContent=urlDefaultText;
 	if(name.value.trim()!==''){
 		url.textContent+=`?name=${name.value}`
